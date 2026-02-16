37669e10bf71c3cb7ec0f24c86130696b9824b6f
Pranshu2404 <pranshu7905@gmail.com>
2026-02-17 04:42:04 +0530

    dotenv removed

--- lib/utility/constants.dart
+++ lib/utility/constants.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF2A2D3E);
 const bgColor = Color(0xFF212332);
 const defaultPadding = 16.0;
 
 // final String MAIN_URL = dotenv.env['MAIN_BACKEND_URL']!;
-final String MAIN_URL = dotenv.env['MAIN_BACKEND_URL']!;
+const String MAIN_URL =
+    String.fromEnvironment('MAIN_BACKEND_URL', defaultValue: 'http://localhost:3000');
